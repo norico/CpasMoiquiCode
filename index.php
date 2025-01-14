6960b07e51abfeeaefacceceaa7223359d2365be
norico <legion.norico@gmail.com>
2025-01-15 00:22:36 +0100

    header and footer

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php get_header(); ?>
-
 <?php if ( have_posts() ) : ?>
     <?php while ( have_posts() ) : the_post(); ?>
         <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-        <?php the_content(); ?>
+        <?php the_excerpt(); ?>
     <?php endwhile; ?>
 <?php endif; ?>
-
 <?php get_footer(); ?>
